6219d05d524e229d50a1f2c2b55467cdf7fb82fc
Dale Bendixen <dalebendixen@gmail.com>
2022-03-17 20:02:19 -0600

    Added onChange callback function to input search bar

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,16 +2,23 @@ import React, { Component } from 'react'
 
 // This Component is a search bar for the user
 // to enter a keyword to search for
+// the styling is coming from semantic ui
 
 
 export class SearchBar extends Component {
+    // function for change in search bar input
+    onInputChange(event) {
+        console.log(event.target.value);
+    }
+
+
   render() {
     return (
       <div className= "ui segment">
           <form className= "ui form" >
             <div className= "field" >
                 <label >Image Search</label>
-                <input type="text" /> 
+                <input type="text" onChange={this.onInputChange} /> 
             </div> 
           </form>
       </div>
